a366bd216486498cefbe24a265b41f0e97a4641c
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-17 17:02:11 +0700

    Menambahkan kelas UserBloc untuk mengelola state dan event pengguna

---
+++ lib/viewmodel/user_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mvvmbloc/viewmodel/user_event.dart';
+import 'package:mvvmbloc/viewmodel/user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  
+}
