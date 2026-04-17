f46d8153e9d5747f2aba6a8fe21f8cfc728d9ecd
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-17 17:09:28 +0700

    Menambahkan dukungan Bloc dengan mengintegrasikan UserBloc dan memperbarui tampilan utama aplikasi untuk menggunakan Homepage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mvvmbloc/view/homepage.dart';
+import 'package:mvvmbloc/viewmodel/user_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return BlocProvider(
+      create: (_) => UserBloc(),
+      child: MaterialApp(
+        home: Homepage(),
       ),
     );
   }
