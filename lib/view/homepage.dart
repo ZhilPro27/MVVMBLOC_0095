f215fde05690f8c1f10f1a3056567757f1f1fb1b
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-17 17:08:12 +0700

    Menambahkan tampilan untuk menampilkan nama dan email pengguna pada Homepage

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mvvmbloc/viewmodel/user_bloc.dart';
 import 'package:mvvmbloc/viewmodel/user_event.dart';
+import 'package:mvvmbloc/viewmodel/user_state.dart';
 
 class Homepage extends StatelessWidget {
   @override
@@ -21,7 +22,15 @@ class Homepage extends StatelessWidget {
             onChanged: (value) {
               context.read<UserBloc>().add(EmailChanged(value));
             },
-          )
+          ),
+
+          SizedBox(height: 20),
+
+          BlocBuilder<UserBloc, UserState>(
+            builder: (context, state) {
+              return Text("Nama: ${state.nama}, Email: ${state.email}");
+            },
+          ),
         ],
       ),
     );
