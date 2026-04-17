1642ca45bd2f21084e7206e356769f6c151949cc
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-17 17:05:51 +0700

    Memperbarui tampilan Homepage dengan menambahkan AppBar dan menghapus placeholder

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("MVVM with BLoC Example"))
+    );
   }
 }
