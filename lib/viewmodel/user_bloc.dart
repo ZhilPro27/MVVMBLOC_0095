be28cd9aaf7edcb519cd9c5422dbf932da15d207
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-17 17:03:53 +0700

    Menambahkan penanganan event NamaChanged pada UserBloc untuk memperbarui nama pengguna

--- lib/viewmodel/user_bloc.dart
+++ lib/viewmodel/user_bloc.dart
@@ -3,5 +3,9 @@ import 'package:mvvmbloc/viewmodel/user_event.dart';
 import 'package:mvvmbloc/viewmodel/user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  
+  UserBloc() : super(UserState()) {
+    on<NamaChanged>((event, emit) {
+      emit(state.copyWith(nama: event.nama));
+    });
+  }
 }
