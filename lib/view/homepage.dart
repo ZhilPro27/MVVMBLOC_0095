aa45774e9c1ccb91992c31f7df9ed1dfe5f80caa
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-17 17:06:52 +0700

    Menambahkan tampilan TextField untuk input nama pengguna pada Homepage

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mvvmbloc/viewmodel/user_bloc.dart';
+import 'package:mvvmbloc/viewmodel/user_event.dart';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("MVVM with BLoC Example"))
+      appBar: AppBar(title: Text("MVVM with BLoC Example")),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: "Nama"),
+            onChanged: (value) {
+              context.read<UserBloc>().add(NamaChanged(value));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
